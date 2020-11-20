e95b334a1b51c31f0769c5ebec2154c470c06809
tingyu00 <zoezhang0408@gmail.com>
2020-11-20 10:20:03 -0800

    Update a function

--- src/download_data.py
+++ src/download_data.py
@@ -6,8 +6,11 @@ Options:
 
 """
 from docopt import docopt
+import pandas as pd
+import os
 
 if __name__ == "__main__":
+    
     args = docopt(__doc__)
     
     import pandas as pd
@@ -18,3 +21,10 @@ if __name__ == "__main__":
     data.to_csv(os.path.join(data_folder, args['<filename>']))
 
 # To use this script with jupyter notebooks to download data into /data folder call: !python download_data.py data raw.csv
+
+def download_data(database_name,filename):
+    url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/'
+    url = os.path.join(url,database_name)
+    url = os.path.join(url,filename)
+    data = pd.read_csv(url, sep=';')
+    return data
