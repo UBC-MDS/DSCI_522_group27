c3794cd7383068a3e1fbcf33cee908fa66d7b541
Mike Lynch <lynchmi@student.ubc.ca>
2020-11-26 13:29:29 -0500

    added command line arguments to data download script

--- src/download_data.py
+++ src/download_data.py
@@ -1,3 +1,12 @@
+""""
+Script for downloading the white wine dataset.
+
+Usage:
+    download_data.py <data_folder> <filename>
+
+Options:
+"""
+from docopt import docopt
 import pandas as pd
 import os
 
@@ -19,3 +28,7 @@ def download_data(data_folder, filename):
     if not os.path.isdir(data_folder):
         os.mkdir(data_folder) 
         data.to_csv(os.path.join(data_folder, filename))
+        
+if __name__ == "__main__":
+    args = docopt(__doc__)
+    download_data(args['<data_folder>'], args['<filename>'])
