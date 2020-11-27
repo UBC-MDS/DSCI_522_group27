74211f9b934c3e7eb225b16b76a20c78dc9ffb1f
Mike Lynch <lynchmi@student.ubc.ca>
2020-11-27 16:50:52 -0500

    added model selection and evaluation script + some formatting on the other scripts

--- src/download_data.py
+++ src/download_data.py
@@ -10,6 +10,7 @@ from docopt import docopt
 import pandas as pd
 import os
 
+
 def download_data(data_folder, filename):
     """Function for downloading the raw white wine quality data and saving as a .csv file
 
@@ -23,13 +24,14 @@ def download_data(data_folder, filename):
     download_data('data','raw_data.csv')
 
    """
-    url = 'http://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-white.csv'
-    data = pd.read_csv(url, sep=';')
+    url = "http://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-white.csv"
+    data = pd.read_csv(url, sep=";")
     if not os.path.isdir(data_folder):
-        os.mkdir(data_folder) 
+        os.mkdir(data_folder)
     data.to_csv(os.path.join(data_folder, filename))
-    
-        
+
+
 if __name__ == "__main__":
     args = docopt(__doc__)
-    download_data(args['<data_folder>'], args['<filename>'])
+    download_data(args["<data_folder>"], args["<filename>"])
+
